929b7f9e67e89e136f6a8e1ab4f1c3472db3ff49
V1a4eslav <dphusevvia4eslav@gmail.com>
2021-12-11 20:37:46 +0200

    class over

--- src/scripts/dom.js
+++ src/scripts/dom.js
@@ -1,9 +1,4 @@
 export const DOM =
 	{
-		sliderLine: document.querySelector('.slider__line'),
-		prev: document.querySelector('.slider__btn_prev'),
-		next: document.querySelector('.slider__btn_next'),
-		pausedBtn: document.querySelector('.slider__btn_paused'),
-		playBtn: document.querySelector('.slider__btn_play'),
-		img: document.querySelector('.slider__img'),
+
 	}
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,52 +1,61 @@
 import '../css/style.css';
-import {DOM} from "./dom";
 
-const widthSliderLine = parseInt(getComputedStyle(DOM.sliderLine).width)
-const widthImg = parseInt(getComputedStyle(DOM.img).width)
-let offset = 0;
-let starting;
-
-DOM.prev.addEventListener('click', togglePrev);
-DOM.next.addEventListener('click', toggleNext);
-DOM.playBtn.addEventListener('click', autoplay);
-DOM.pausedBtn.addEventListener('click', pausedSlider);
-
-function togglePrev() {
-	offset = offset - widthImg;
-	if (offset < 0) {
-		offset = (widthSliderLine - widthImg);
+class Slider {
+	constructor() {
+		this.sliderLine = document.querySelector('.slider__line'),
+			this.prev = document.querySelector('.slider__btn_prev'),
+			this.next = document.querySelector('.slider__btn_next'),
+			this.pausedBtn = document.querySelector('.slider__btn_paused'),
+			this.playBtn = document.querySelector('.slider__btn_play'),
+			this.img = document.querySelector('.slider__img'),
+			this.prev.addEventListener('click', this.togglePrev.bind(this)),
+			this.next.addEventListener('click', this.toggleNext.bind(this)),
+			this.playBtn.addEventListener('click', this.autoplay.bind(this)),
+			this.pausedBtn.addEventListener('click', this.pausedSlider.bind(this)),
+			this.offset = 0,
+			this.starting = 0,
+			this.widthSliderLine = parseInt(getComputedStyle(this.sliderLine).width),
+			this.widthImg = parseInt(getComputedStyle(this.img).width)
 	}
-	DOM.sliderLine.style.left = -offset + 'px';
-}
 
-function toggleNext() {
-	offset = offset + widthImg;
-	if (offset === widthSliderLine) {
-		offset = 0;
+	togglePrev() {
+		this.offset = this.offset - this.widthImg;
+		if (this.offset < 0) {
+			this.offset = (this.widthSliderLine - this.widthImg);
+		}
+		this.sliderLine.style.left = -this.offset + 'px';
 	}
-	DOM.sliderLine.style.left = -offset + 'px';
+
+	toggleNext() {
+		this.offset = this.offset + this.widthImg;
+		if (this.offset === this.widthSliderLine) {
+			this.offset = 0;
+		}
+		this.sliderLine.style.left = -this.offset + 'px';
+	}
+
+	pausedSlider() {
+		clearInterval(this.starting);
+		this.disabledTrueBtn(this.pausedBtn);
+		this.disabledFalseBtn(this.playBtn);
+	}
+
+	autoplay() {
+		this.starting = setInterval(this.toggleNext.bind(this), 1500);
+		this.disabledTrueBtn(this.playBtn);
+		this.disabledFalseBtn(this.pausedBtn);
+	}
+
+	disabledFalseBtn(btn) {
+		btn.disabled = false;
+	}
+
+	disabledTrueBtn(btn) {
+		btn.disabled = true;
+	}
 }
 
-function pausedSlider() {
-	clearInterval(starting);
-	disabledTrueBtn(DOM.pausedBtn);
-	disabledFalseBtn(DOM.playBtn);
-}
-
-function autoplay() {
-	starting = setInterval(toggleNext, 1500);
-	disabledTrueBtn(DOM.playBtn);
-	disabledFalseBtn(DOM.pausedBtn);
-}
-
-function disabledFalseBtn(btn) {
-	btn.disabled = false;
-}
-
-function disabledTrueBtn(btn) {
-	btn.disabled = true;
-}
-
-autoplay();
+const slider = new Slider();
+slider.autoplay();
 
 
